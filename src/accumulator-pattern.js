a920ccbd9b8a141e9285de9a1a7ce4b84a591283
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-11-26 18:15:12 -0500

    Finish accumulator-pattern

--- src/accumulator-pattern.js
+++ src/accumulator-pattern.js
@@ -3,7 +3,7 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+const isValid = (rolls) => rolls.every(roll => typeof roll === 'number')
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +11,7 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+const findValue = (rolls, value) => rolls.find(roll => roll === value) || null
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +19,19 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+ const filterOutLowValues = (rolls, lowest) => rolls.filter(roll => roll >= lowest)
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+
+ const getRollCounts = (rolls) => {
+  let objRolls = {}
+  rolls.forEach(roll => objRolls[roll] ? objRolls[roll]++ : objRolls[roll] = 1)
+  return objRolls
+ }
 
 // Do not change the code below here.
 module.exports = {
