6bf94dc9f6c5689bb7663aa6a283b64b642da9dd
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-11-27 01:25:10 -0500

    Finish dinosaur facts

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -22,7 +22,13 @@ const exampleDinosaurData = require("../data/dinosaurs");
  *  getLongestDinosaur(dinosaurs);
  *  //> { Brachiosaurus: 98.43 }
  */
-function getLongestDinosaur(dinosaurs) {}
+const getLongestDinosaur = (dinosaurs) => {
+  if (!dinosaurs.length) {
+    return {}
+  }
+  let longDino = dinosaurs.reduce((accumulator, currentDino) => accumulator.lengthInMeters < currentDino.lengthInMeters ? currentDino : accumulator)
+  return ({[longDino.name]: longDino.lengthInMeters * 3.281})
+}
 
 /**
  * getDinosaurDescription()
@@ -44,19 +50,32 @@ function getLongestDinosaur(dinosaurs) {}
  *  getDinosaurDescription(dinosaurs, "incorrect-id");
  *  //> "A dinosaur with an ID of 'incorrect-id' cannot be found."
  */
-function getDinosaurDescription(dinosaurs, id) {}
+ const getDinosaurDescription = (dinosaurs, id) => {
+
+    let findDino =  dinosaurs.find( dino => dino.dinosaurId === id )
+
+    if(!findDino){
+      throw `A dinosaur with an ID of 'incorrect-id' cannot be found.`
+    }
+
+    return `${findDino.name} (${findDino.pronunciation})\n${findDino.info} It lived in the ${findDino.period} period, over ${findDino.mya[findDino.mya.length-1]} million years ago.`
+ }
 
 /**
  * getDinosaursAliveMya()
  * ---------------------
- * Returns an array of dinosaurs who were alive at the given `mya` (i.e. "millions of years ago") value. If a `key` is provided, returns the value of that key for each dinosaur alive at that time. Otherwise, returns the ID.
+ * Returns an array of dinosaurs who were alive at the given `mya` (i.e. "millions of years ago") value. 
+ * If a `key` is provided, returns the value of that key for each dinosaur alive at that time. Otherwise, returns the ID.
  *
- * If the dinosaur only has a single value for `mya`, allows for the `mya` value to be equal to the given value or one less. For example, if a dinosaur has a `mya` value of `[29]`, the dinosaur's information will be returned if `29` is entered or `28` is entered.
+ * If the dinosaur only has a single value for `mya`, allows for the `mya` value to be equal to the given value or one less.
+ *  For example, if a dinosaur has a `mya` value of `[29]`, the dinosaur's information will be returned if `29` is entered or `28` is entered.
  *
  * @param {Object[]} dinosaurs - An array of dinosaur objects. See the `data/dinosaurs.js` file for an example of the input.
  * @param {number} mya - "Millions of years ago."
- * @param {string} key - An optional parameter. If included, for dinosaurs that lived during the `mya` value given, will return the value of the supplied key. Otherwise, returns the ID.
- * @returns {*[]} An array of values, which depend on the key given. The array should only include data of dinosaurs who lived during the given time period.
+ * @param {string} key - An optional parameter. If included, for dinosaurs that lived during the `mya` value given, will return 
+ * the value of the supplied key. Otherwise, returns the ID.
+ * @returns {*[]} An array of values, which depend on the key given. The array should only include data of dinosaurs who lived during 
+ * the given time period.
  *
  * EXAMPLE:
  *  getDinosaursAliveMya(dinosaurs, 150);
@@ -71,8 +90,26 @@ function getDinosaurDescription(dinosaurs, id) {}
  *  getDinosaursAliveMya(dinosaurs, 65, "unknown-key");
  *  //> ["WHQcpcOj0G"]
  */
-function getDinosaursAliveMya(dinosaurs, mya, key) {}
-
+const getDinosaursAliveMya = (dinosaurs, mya, key) => {
+  let dinoArray =[]
+  dinosaurs.forEach(dino => {
+    if(dino.mya.length === 1 && dino.mya[0] === mya || dino.mya[0] - 1 === mya){
+      if(!key){
+        dinoArray.push(dino.dinosaurId)
+      } else {
+        dinoArray.push(dino[key])
+      }
+    } else if(dino.mya.length > 1 && dino.mya[0] >= mya && dino.mya[dino.mya.length-1] <= mya ){
+        if(!key){
+          dinoArray.push(dino.dinosaurId)
+        } else {
+            dinoArray.push(dino[key])
+        }
+      }
+  })
+  return dinoArray
+}
+    
 module.exports = {
   getLongestDinosaur,
   getDinosaurDescription,
